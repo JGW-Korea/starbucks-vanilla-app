8f17ebf64cf3ac75b2fb6bec466bf2b6a0027c9e
JGW-Korea <jgw6372@gmail.com>
2024-08-03 19:10:47 +0900

    다중 요소 슬라이드

--- js/main.js
+++ js/main.js
@@ -84,6 +84,18 @@ new Swiper(".promotion .swiper", {
   },
 });
 
+new Swiper(".awards .swiper", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+
+  navigation: {
+    nextEl: ".awards .swiper-button-next",
+    prevEl: ".awards .swiper-button-prev",
+  },
+});
+
 const promotionElement = document.querySelector(".promotion");
 const promotionToggleBtn = document.querySelector(".notice .toggle-promtion");
 
